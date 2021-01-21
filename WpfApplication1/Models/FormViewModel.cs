b01418475faf6b31bc295b9107ec8aba8aa32762
ADiks <antondikson@gmail.com>
2021-01-21 18:10:54 +0200

    replace

--- WpfApplication1/Models/FormViewModel.cs
+++ WpfApplication1/Models/FormViewModel.cs
@@ -2,6 +2,8 @@
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using WpfApplication1.Annotations;
 using WpfApplication1.Services;
 
 
@@ -9,18 +11,31 @@ namespace WpfApplication1.Models
 {
     public class FormViewModel : INotifyPropertyChanged
     {
-        private const string BaseUrl = @"https://habr.com/ru/";
+
+        
         private int _pageCounter;
 
+        private int _postCount;
+
+        public int PostCount
+        {
+            get => _postCount;
+            set
+            {
+                _postCount = value;
+                OnPropertyChanged();
+            }
+        }
+
         private string _input;
-        
+
         public string Input
         {
             get => _input;
             set
             {
                 _input = value;
-                OnPropertyChanged("Input");
+                OnPropertyChanged();
             }
         }
 
@@ -32,66 +47,78 @@ namespace WpfApplication1.Models
             set
             {
                 _pages = value;
-                OnPropertyChanged("Pages");
+                OnPropertyChanged();
             }
         }
-
 
         public ObservableCollection<NewsViewModel> News { get; set; } = new ObservableCollection<NewsViewModel>();
 
         private NewsViewModel _selectedNews;
-        
+
         public NewsViewModel SelectedNews
         {
             get => _selectedNews;
             set
             {
                 _selectedNews = value;
-                OnPropertyChanged("SelectedNews");
+                OnPropertyChanged();
             }
         }
 
         private readonly Parser _parser;
-    
+
         public FormViewModel()
         {
-            _parser =  new Parser(BaseUrl);;
-            LoadData();
+            _parser = new Parser(BaseUrl);
+            LoadAsync();
         }
-        
-        private void LoadData()
+
+        private ObservableCollection<NewsViewModel> LoadData()
         {
+            var collection = new ObservableCollection<NewsViewModel>();
+
             _parser.Load($"page{++_pageCounter}/");
             var document = _parser.Document;
             
-            var pagination = Parser.Qs(document, "ul.toggle-menu_pagination", true);
+            Pages = "Your upload: " + _pageCounter;
 
-            Pages = "Your upload: " + _pageCounter + ", total: " +
-                    Parser.QsAttr(pagination, "a.toggle-menu__item-link_bordered", "href")
-                          ?.Substring(8).TrimEnd('/');
-        
-            foreach(var item in Parser.QsAll(document, "article.post"))
+            foreach (var item in Parser.QsAll(document, "article.post"))
             {
+                PostCount++;
                 var postHeader = Parser.Qs(item, "header.post__meta", true);
-                var postText = Parser.QsAll(Parser.Qs(item, "div.post__text", true), "p");
-                var description = postText.Aggregate("", (current, node) => current + "\n" + node.InnerHtml);
                 
-                News.Add(new NewsViewModel
+                collection.Add(new NewsViewModel
                 {
                     Author = Parser.QsAttr(postHeader, "a.post__user-info", "href"),
                     Date = Parser.Qs(postHeader, "span.post__time"),
                     Title = Parser.Qs(item, "a.post__title_link"),
-                    Description = description,
+                    Description = Parser.Qs(item, "div.post__text", true)?.InnerText ?? "text",
                     Link = Parser.QsAttr(Parser.Qs(item, "div.post__body", true), "a.btn", "href")
                 });
             }
+
+            return collection;
         }
 
-        public RelayCommand NextPage => new RelayCommand(obj => { LoadData(); });
-        
+        private async void LoadAsync()
+        {
+            while (true)
+            {
+                ObservableCollection<NewsViewModel> collection = await Task.Run(LoadData);
+                foreach (var newsViewModel in collection) News.Add(newsViewModel);
+                if (_pageCounter != 50) continue;
+                break;
+            }
+        }
+
+        public RelayCommand NextPage => new RelayCommand(obj => { LoadAsync(); });
+
         public event PropertyChangedEventHandler PropertyChanged;
 
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
